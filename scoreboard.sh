6eeffa79401082f4ac4b3ace049dd1fb4432b39f
Uditha de Soysa <Uditha.deSoysa@sa.gov.au>
2017-11-22 09:18:30 +1030

    Fix the URL redirection

--- scoreboard.sh
+++ scoreboard.sh
@@ -21,16 +21,19 @@ URL=http://www.espncricinfo.com/ci/engine/match/$1.html?view=scorecard;wrapperty
 # Show match summary
 printf "Match Summary\n---------------------\n"
 # Title
-curl -s $URL| grep 'team-1\|team-2' | awk -v FS="(\">|<)" '{print $3$5}'
-curl -s $URL| grep "innings-requirement" | awk -v FS="(\">|<)" '{print $3$5}'
+curl -L -s $URL| grep 'team-1\|team-2' | awk -v FS="(\">|<)" '{print $3$5}'
+curl -L -s $URL| grep "innings-requirement" | awk -v FS="(\">|<)" '{print $3$5}'
+curl -L -s $URL| grep "Close of play" -A 10 | grep "\<li\>" | awk -v FS="(\">|<)" '{print $4$6","$9","$12")"}' 
+#curl -s "http://www.espncricinfo.com/ci/engine/match/1000885.html?view=commentary;wrappertype=none" 2>&1| grep -i "commentary-section" -A 3 | grep "<b>" | awk -v FS="(b>|<)" '{print $3"|"$5}'
+
 echo ""
 # Summary
-curl -s http://www.espncricinfo.com/ci/engine/match/$1.html | grep \<title\> | cut -d">" -f2 | cut -d"|" -f 1 |  sed -e "s/,/\n/g"  -e "s/(/| /g" -e "s/)//g" | sed -e "s/^ //g" |awk  -F- '{print $1 "\033[1;31m" $2 "\033[0m"}'
+curl -L -s http://www.espncricinfo.com/ci/engine/match/$1.html | grep \<title\> | cut -d">" -f2 | cut -d"|" -f 1 |  sed -e "s/,/\n/g"  -e "s/(/| /g" -e "s/)//g" | sed -e "s/^ //g" |awk  -F- '{print $1 "\033[1;31m" $2 "\033[0m"}'
 
 
 # Show full scoreboard
 printf "\nFull Scoreboard\n----------------------\n"
-curl -s $URL| grep -A3 'bowling-table\|batsman-name' \
+curl -L -s $URL| grep -A3 'bowling-table\|batsman-name' \
 				| sed -e "s/bowling-table/batsman-name\ndismissal-info\nbold\ntd\ class=\"\"\n/g" \
 				| awk -F">" '/batsman-name/ {b=$3} /dismissal-info/ {d=$2} /bold/ {a=$2} /td class=""/ {c=$2; print "|"b"|"d"|"a"|"c}' \
 				|sed -e "s/\/td|//g" -e "s/\/a//g" -e "s/|//g" \
